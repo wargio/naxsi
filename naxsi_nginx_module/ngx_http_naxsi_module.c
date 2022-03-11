4d85d3e6e023c24e384fff18df2434846cda8116
wargio <wargio@libero.it>
2022-03-11 01:18:59 +0100

    Small cleanup

--- naxsi_nginx_module/ngx_http_naxsi_module.c
+++ naxsi_nginx_module/ngx_http_naxsi_module.c
@@ -10,6 +10,8 @@
 #include <ngx_http_core_module.h>
 #include <naxsi.h>
 
+typedef char *(*cmd_set_cb)(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+
 #define ngx_naxsi_return_val_if(x, y) \
 	do { \
 		if (!(x)) { \
@@ -21,137 +23,6 @@
 #define ngx_naxsi_arg(s) \
 	{ (char *)s.data, s.len }
 #define ngx_naxsi_memory(ppool) naxsi_memory(ppool, &nginx_naxsi_free, &nginx_naxsi_malloc, &nginx_naxsi_calloc)
-
-extern ngx_module_t ngx_http_naxsi_module;
-
-static ngx_int_t ngx_http_naxsi_post_configuration(ngx_conf_t *cf);
-static void *ngx_http_naxsi_create_naxsi(ngx_conf_t *cf);
-
-static char *ngx_http_naxsi_main_rule(ngx_conf_t *cf, ngx_command_t *cmd, void *local);
-static char *ngx_http_naxsi_basic_rule(ngx_conf_t *cf, ngx_command_t *cmd, void *local);
-static char *ngx_http_naxsi_denied_url(ngx_conf_t *cf, ngx_command_t *cmd, void *local);
-static char *ngx_http_naxsi_ignore_ip_request(ngx_conf_t *cf, ngx_command_t *cmd, void *local);
-static char *ngx_http_naxsi_ignore_cidr_request(ngx_conf_t *cf, ngx_command_t *cmd, void *local);
-static char *ngx_http_naxsi_check_rule(ngx_conf_t *cf, ngx_command_t *cmd, void *local);
-static char *ngx_http_naxsi_flags(ngx_conf_t *cf, ngx_command_t *cmd, void *local);
-
-static ngx_command_t ngx_http_naxsi_commands[] = {
-	/* MainRule */
-	{ ngx_string(NAXSI_KEYWORD_MAIN_RULE),
-		NGX_HTTP_MAIN_CONF | NGX_CONF_1MORE,
-		ngx_http_naxsi_main_rule,
-		NGX_HTTP_MAIN_CONF_OFFSET,
-		0,
-		NULL },
-
-	/* BasicRule */
-	{ ngx_string(NAXSI_KEYWORD_BASIC_RULE),
-		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_1MORE,
-		ngx_http_naxsi_basic_rule,
-		NGX_HTTP_LOC_CONF_OFFSET,
-		0,
-		NULL },
-
-	/* DeniedUrl */
-	{ ngx_string(NAXSI_KEYWORD_DENIED_URL),
-		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_1MORE,
-		ngx_http_naxsi_denied_url,
-		NGX_HTTP_LOC_CONF_OFFSET,
-		0,
-		NULL },
-
-	/* IgnoreIP */
-	{ ngx_string(NAXSI_KEYWORD_IGNORE_IP),
-		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_1MORE,
-		ngx_http_naxsi_ignore_ip_request,
-		NGX_HTTP_LOC_CONF_OFFSET,
-		0,
-		NULL },
-
-	/* IgnoreCIDR */
-	{ ngx_string(NAXSI_KEYWORD_IGNORE_CIDR),
-		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_1MORE,
-		ngx_http_naxsi_ignore_cidr_request,
-		NGX_HTTP_LOC_CONF_OFFSET,
-		0,
-		NULL },
-
-	/* CheckRule */
-	{ ngx_string(NAXSI_KEYWORD_CHECK_RULE),
-		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_1MORE,
-		ngx_http_naxsi_check_rule,
-		NGX_HTTP_LOC_CONF_OFFSET,
-		0,
-		NULL },
-
-	/* LearningMode */
-	{ ngx_string(NAXSI_KEYWORD_LEARNING_FLAG),
-		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
-		ngx_http_naxsi_flags,
-		NGX_HTTP_LOC_CONF_OFFSET,
-		0,
-		NULL },
-
-	/* SecRulesEnabled */
-	{ ngx_string(NAXSI_KEYWORD_ENABLED_FLAG),
-		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
-		ngx_http_naxsi_flags,
-		NGX_HTTP_LOC_CONF_OFFSET,
-		0,
-		NULL },
-
-	/* SecRulesDisabled */
-	{ ngx_string(NAXSI_KEYWORD_DISABLED_FLAG),
-		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
-		ngx_http_naxsi_flags,
-		NGX_HTTP_LOC_CONF_OFFSET,
-		0,
-		NULL },
-
-	/* LibInjectionSql */
-	{ ngx_string(NAXSI_KEYWORD_LIBINJECTION_SQL),
-		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
-		ngx_http_naxsi_flags,
-		NGX_HTTP_LOC_CONF_OFFSET,
-		0,
-		NULL },
-
-	/* LibInjectionXss */
-	{ ngx_string(NAXSI_KEYWORD_LIBINJECTION_XSS),
-		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
-		ngx_http_naxsi_flags,
-		NGX_HTTP_LOC_CONF_OFFSET,
-		0,
-		NULL },
-
-	ngx_null_command
-};
-
-static ngx_http_module_t ngx_http_naxsi_module_context = {
-	NULL, /* preconfiguration */
-	ngx_http_naxsi_post_configuration, /* postconfiguration */
-	ngx_http_naxsi_create_naxsi, /* create main configuration */
-	NULL, /* init main configuration */
-	NULL, /* create server configuration */
-	NULL, /* merge server configuration */
-	ngx_http_naxsi_create_naxsi, /* create location configuration */
-	NULL /* merge location configuration */
-};
-
-ngx_module_t ngx_http_naxsi_module = {
-	NGX_MODULE_V1,
-	&ngx_http_naxsi_module_context, /* module context */
-	ngx_http_naxsi_commands, /* module directives */
-	NGX_HTTP_MODULE, /* module type */
-	NULL, /* init master */
-	NULL, /* init module */
-	NULL, /* init process */
-	NULL, /* init thread */
-	NULL, /* exit thread */
-	NULL, /* exit process */
-	NULL, /* exit master */
-	NGX_MODULE_V1_PADDING
-};
 
 static void nginx_naxsi_free(ngx_pool_t *pool, void *ptr) {
 	ngx_pfree(pool, ptr);
@@ -194,52 +65,170 @@ static void *ngx_http_naxsi_create_naxsi(ngx_conf_t *cf) {
 	return naxsi_new(&mem);
 }
 
-static char *ngx_http_naxsi_main_rule(ngx_conf_t *cf, ngx_command_t *cmd, void *local) {
+static char *ngx_http_naxsi_main_rule(ngx_conf_t *cf, ngx_command_t *cmd, naxsi_t *naxsi_loc) {
 	return NGX_CONF_ERROR;
 }
 
-static char *ngx_http_naxsi_basic_rule(ngx_conf_t *cf, ngx_command_t *cmd, void *local) {
+static char *ngx_http_naxsi_basic_rule(ngx_conf_t *cf, ngx_command_t *cmd, naxsi_t *naxsi_loc) {
 	return NGX_CONF_ERROR;
 }
 
-static char *ngx_http_naxsi_denied_url(ngx_conf_t *cf, ngx_command_t *cmd, void *local) {
+static char *ngx_http_naxsi_denied_url(ngx_conf_t *cf, ngx_command_t *cmd, naxsi_t *naxsi_loc) {
 	return NGX_CONF_ERROR;
 }
 
-static char *ngx_http_naxsi_ignore_ip_request(ngx_conf_t *cf, ngx_command_t *cmd, void *local) {
-	ngx_naxsi_return_val_if(cf && local, NGX_CONF_ERROR);
+static char *ngx_http_naxsi_ignore_ip_request(ngx_conf_t *cf, ngx_command_t *cmd, naxsi_t *naxsi_loc) {
+	ngx_naxsi_return_val_if(cf && naxsi_loc, NGX_CONF_ERROR);
 
 	ngx_str_t *value = cf->args->elts;
-	naxsi_t *naxsi = (naxsi_t *)local;
 	naxsi_mem_t mem = ngx_naxsi_memory(cf->pool);
 	naxsi_str_t ip_address = ngx_naxsi_arg(value[1]);
 
-	if (!naxsi_ignore_ip(&mem, naxsi, &ip_address)) {
+	if (!naxsi_ignore_ip(&mem, naxsi_loc, &ip_address)) {
 		ngx_naxsi_error("invalid " NAXSI_KEYWORD_IGNORE_IP " value: %s", (const char *)value[1].data);
 		return NGX_CONF_ERROR;
 	}
 	return NGX_OK;
 }
 
-static char *ngx_http_naxsi_ignore_cidr_request(ngx_conf_t *cf, ngx_command_t *cmd, void *local) {
-	ngx_naxsi_return_val_if(cf && local, NGX_CONF_ERROR);
+static char *ngx_http_naxsi_ignore_cidr_request(ngx_conf_t *cf, ngx_command_t *cmd, naxsi_t *naxsi_loc) {
+	ngx_naxsi_return_val_if(cf && naxsi_loc, NGX_CONF_ERROR);
 
 	ngx_str_t *value = cf->args->elts;
-	naxsi_t *naxsi = (naxsi_t *)local;
 	naxsi_mem_t mem = ngx_naxsi_memory(cf->pool);
 	naxsi_str_t cidr = ngx_naxsi_arg(value[1]);
 
-	if (!naxsi_ignore_cidr(&mem, naxsi, &cidr)) {
+	if (!naxsi_ignore_cidr(&mem, naxsi_loc, &cidr)) {
 		ngx_naxsi_error("invalid " NAXSI_KEYWORD_IGNORE_CIDR " value: %s", (const char *)value[1].data);
 		return NGX_CONF_ERROR;
 	}
 	return NGX_OK;
 }
 
-static char *ngx_http_naxsi_check_rule(ngx_conf_t *cf, ngx_command_t *cmd, void *local) {
+static char *ngx_http_naxsi_check_rule(ngx_conf_t *cf, ngx_command_t *cmd, naxsi_t *naxsi_loc) {
 	return NGX_CONF_ERROR;
 }
 
-static char *ngx_http_naxsi_flags(ngx_conf_t *cf, ngx_command_t *cmd, void *local) {
+static char *ngx_http_naxsi_flags(ngx_conf_t *cf, ngx_command_t *cmd, naxsi_t *naxsi_loc) {
 	return NGX_CONF_ERROR;
 }
+
+extern ngx_module_t ngx_http_naxsi_module;
+
+static ngx_command_t ngx_http_naxsi_commands[] = {
+	/* MainRule */
+	{ ngx_string(NAXSI_KEYWORD_MAIN_RULE),
+		NGX_HTTP_MAIN_CONF | NGX_CONF_1MORE,
+		(cmd_set_cb)ngx_http_naxsi_main_rule,
+		NGX_HTTP_MAIN_CONF_OFFSET,
+		0,
+		NULL },
+
+	/* BasicRule */
+	{ ngx_string(NAXSI_KEYWORD_BASIC_RULE),
+		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_1MORE,
+		(cmd_set_cb)ngx_http_naxsi_basic_rule,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		0,
+		NULL },
+
+	/* DeniedUrl */
+	{ ngx_string(NAXSI_KEYWORD_DENIED_URL),
+		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_1MORE,
+		(cmd_set_cb)ngx_http_naxsi_denied_url,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		0,
+		NULL },
+
+	/* IgnoreIP */
+	{ ngx_string(NAXSI_KEYWORD_IGNORE_IP),
+		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_1MORE,
+		(cmd_set_cb)ngx_http_naxsi_ignore_ip_request,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		0,
+		NULL },
+
+	/* IgnoreCIDR */
+	{ ngx_string(NAXSI_KEYWORD_IGNORE_CIDR),
+		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_1MORE,
+		(cmd_set_cb)ngx_http_naxsi_ignore_cidr_request,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		0,
+		NULL },
+
+	/* CheckRule */
+	{ ngx_string(NAXSI_KEYWORD_CHECK_RULE),
+		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_1MORE,
+		(cmd_set_cb)ngx_http_naxsi_check_rule,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		0,
+		NULL },
+
+	/* LearningMode */
+	{ ngx_string(NAXSI_KEYWORD_LEARNING_FLAG),
+		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
+		(cmd_set_cb)ngx_http_naxsi_flags,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		0,
+		NULL },
+
+	/* SecRulesEnabled */
+	{ ngx_string(NAXSI_KEYWORD_ENABLED_FLAG),
+		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
+		(cmd_set_cb)ngx_http_naxsi_flags,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		0,
+		NULL },
+
+	/* SecRulesDisabled */
+	{ ngx_string(NAXSI_KEYWORD_DISABLED_FLAG),
+		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
+		(cmd_set_cb)ngx_http_naxsi_flags,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		0,
+		NULL },
+
+	/* LibInjectionSql */
+	{ ngx_string(NAXSI_KEYWORD_LIBINJECTION_SQL),
+		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
+		(cmd_set_cb)ngx_http_naxsi_flags,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		0,
+		NULL },
+
+	/* LibInjectionXss */
+	{ ngx_string(NAXSI_KEYWORD_LIBINJECTION_XSS),
+		NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
+		(cmd_set_cb)ngx_http_naxsi_flags,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		0,
+		NULL },
+
+	ngx_null_command
+};
+
+static ngx_http_module_t ngx_http_naxsi_module_context = {
+	NULL, /* preconfiguration */
+	ngx_http_naxsi_post_configuration, /* postconfiguration */
+	ngx_http_naxsi_create_naxsi, /* create main configuration */
+	NULL, /* init main configuration */
+	NULL, /* create server configuration */
+	NULL, /* merge server configuration */
+	ngx_http_naxsi_create_naxsi, /* create location configuration */
+	NULL /* merge location configuration */
+};
+
+ngx_module_t ngx_http_naxsi_module = {
+	NGX_MODULE_V1,
+	&ngx_http_naxsi_module_context, /* module context */
+	ngx_http_naxsi_commands, /* module directives */
+	NGX_HTTP_MODULE, /* module type */
+	NULL, /* init master */
+	NULL, /* init module */
+	NULL, /* init process */
+	NULL, /* init thread */
+	NULL, /* exit thread */
+	NULL, /* exit process */
+	NULL, /* exit master */
+	NGX_MODULE_V1_PADDING
+};
