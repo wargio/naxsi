dfe7381bbc54469396f17e068efd684763512298
alex <alex@staticlibs.net>
2022-10-17 20:59:43 +0100

    reformat added files with clang-format

--- naxsi_src/naxsi_windows.h
+++ naxsi_src/naxsi_windows.h
@@ -8,7 +8,8 @@
 #include <sys/types.h>
 #include <winsock2.h>
 
-int gettimeofday(struct timeval* t,void* timezone);
+int
+gettimeofday(struct timeval* t, void* timezone);
 
 // from linux's sys/times.h
 
@@ -17,38 +18,40 @@ int gettimeofday(struct timeval* t,void* timezone);
 #define __need_clock_t
 #include <time.h>
 
-#define uint ngx_uint_t
-#define random rand
+#define uint        ngx_uint_t
+#define random      rand
 #define strncasecmp _strnicmp
-#define strcasecmp _stricmp
-#define srandom srand
-#define getpid _getpid
+#define strcasecmp  _stricmp
+#define srandom     srand
+#define getpid      _getpid
 
 // From http://www.linuxjournal.com/article/5574
 
 /* Structure describing CPU time used by a process and its children.  */
 struct tms
 {
-  clock_t tms_utime;          /* User CPU time.  */
-  clock_t tms_stime;          /* System CPU time.  */
+  clock_t tms_utime; /* User CPU time.  */
+  clock_t tms_stime; /* System CPU time.  */
 
-  clock_t tms_cutime;         /* User CPU time of dead children.  */
-  clock_t tms_cstime;         /* System CPU time of dead children.  */
+  clock_t tms_cutime; /* User CPU time of dead children.  */
+  clock_t tms_cstime; /* System CPU time of dead children.  */
 };
 
 /* Store the CPU time used by this process and all its
    dead children (and their dead children) in BUFFER.
    Return the elapsed real time, or (clock_t) -1 for errors.
    All times are in CLK_TCKths of a second.  */
-clock_t times(struct tms *__buffer);
+clock_t
+times(struct tms* __buffer);
 
-typedef long long suseconds_t ;
-
+typedef long long suseconds_t;
 
 // From https://stackoverflow.com/a/20816961
 
-int inet_pton(int af, const char *src, void *dst);
+int
+inet_pton(int af, const char* src, void* dst);
 
-const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
+const char*
+inet_ntop(int af, const void* src, char* dst, socklen_t size);
 
 #endif // __NAXSI_WINDOWS_H__
